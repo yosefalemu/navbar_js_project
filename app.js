f51bd89e9f8650bf15d4a05c3de961f745e7c1de
Yosef Alemu <yosefalemumengstie@gmail.com>
2023-03-01 14:23:30 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const btn_toggle = document.querySelector(".nav-toggle");
+const show_links = document.querySelector(".links");
+btn_toggle.addEventListener("click", function(){
+    if (show_links.className == "links") {
+        show_links.className = "show-link";
+    } 
+    else {
+        show_links.className = "links";
+}
+});
+
